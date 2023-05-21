de2f52d954851884c2f762fe6d9c9e3ea1f6a4f7
IkerMB8 <ikermbergua@gmail.com>
2023-05-21 15:12:49 +0200

    arreglo ruta services

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -37,7 +37,7 @@ Route::get('user/cliente', [ClienteController::class, 'obtenerClienteDeUsuario']
 Route::post('/clientes/{cliente}', [ClienteController::class, 'update_workaround']);
 
 Route::apiResource('reservas', ReservaController::class);
-Route::get('/users/{user}/reservas', [ReservaController::class, 'indexByUser']);
+Route::get('/user/reservas', [ReservaController::class, 'indexByUser']);
 Route::get('/dia/reservas', [ReservaController::class, 'getReservasDia']);
 Route::post('/reservas/{reserva}', [ReservaController::class, 'update_workaround']);
 
